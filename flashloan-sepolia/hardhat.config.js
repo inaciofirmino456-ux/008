d15005779415443193d1197ca4b88eb9b6893b14
inaciofirmino456-ux <inaciofirmino456@gmail.com>
2026-09-25 22:13:59 +0100

    Add Sepolia deploy automation: flashloan-sepolia/hardhat.config.js

---
+++ flashloan-sepolia/hardhat.config.js
@@ -0,0 +1,15 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+const accounts = process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [];
+
+module.exports = {
+  solidity: "0.8.20",
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL || "",
+      chainId: 11155111,
+      accounts
+    }
+  }
+};
